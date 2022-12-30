0c612954023d7dc96aa112f54df06683c19a4445
admin <administrator@jits-3-5.jits.ipa.dc>
2022-12-30 19:39:47 +0600

    methods-fields and updated main method at interfaces

--- src/dev/java/oop/abstraction/Main.java
+++ src/dev/java/oop/abstraction/Main.java
@@ -3,9 +3,8 @@ package dev.java.oop.abstraction;
 public class Main {
     public static void main(String[] args) {
         Robocop robocop = new Robocop();
+        robocop.setName("robo");
         robocop.flight();
-        Terminator terminator = new Terminator();
-        terminator.move();
-        System.out.println("Hello world!");
+        System.out.println("robocop.getName(\"asdsa\") = " + robocop.getName());
     }
 }
