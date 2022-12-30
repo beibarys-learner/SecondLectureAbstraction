3863a6f39080d35979fb4406eae1590cf6f7698a
admin <administrator@jits-3-5.jits.ipa.dc>
2022-12-30 16:33:12 +0600

    first commit

---
+++ src/dev/java/oop/abstraction/IRobot.java
@@ -0,0 +1,7 @@
+package dev.java.oop.abstraction;
+
+public interface IRobot {
+    void move();
+    void stop();
+    void fight();
+}
---
+++ src/dev/java/oop/abstraction/Main.java
@@ -0,0 +1,7 @@
+package dev.java.oop.abstraction;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
